53f8ce75134dec994ca881efef3dcb092d6f43c0
afganirw07 <afganirwansyah070708@gmail.com>
2026-05-07 21:53:12 +0700

    feat: splash screens

---
+++ app/src/main/java/com/example/laporkita/ui/splash/SplashActivity.kt
@@ -0,0 +1,49 @@
+package com.example.laporkita.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import com.example.laporkita.R
+import com.example.laporkita.ui.home.MainActivity
+
+class SplashActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SplashScreen {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(onNavigate: () -> Unit) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onNavigate()
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier.size(550.dp)
+        )
+    }
+}
